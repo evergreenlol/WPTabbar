db2c0581b03900334f6391f885b5b1250361ab56
wp <wpevergreen@163.com>
2017-10-25 09:32:55 +0800

    first commit

---
+++ WPTabbarCustomized/FourViewController.swift
@@ -0,0 +1,35 @@
+//
+//  FourViewController.swift
+//  WPTabbarCustomized
+//
+//  Created by wp on 2017/10/23.
+//  Copyright © 2017年 wp. All rights reserved.
+//
+
+import UIKit
+
+class FourViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ WPTabbarCustomized/WPTabbar.swift
@@ -0,0 +1,21 @@
+//
+//  WPTabbar.swift
+//  WPTabbarCustomized
+//
+//  Created by wp on 2017/10/23.
+//  Copyright © 2017年 wp. All rights reserved.
+//
+
+import UIKit
+
+class WPTabbar: UITabBar {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
